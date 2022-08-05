c20f403cd54eda5580faf00f65eccccbc1238298
Sahal VA <sah.sahal7@gmail.com>
2022-08-05 09:07:15 +0530

    Adding trusted host patterns

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -730,6 +730,11 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * @see https://www.drupal.org/docs/installing-drupal/trusted-host-settings
  */
 
+$settings['trusted_host_patterns'] = [
+  '^www\.example\.com$',
+  'drupal9.lndo.site',
+];
+
 /**
  * The default list of directories that will be ignored by Drupal's file API.
  *
